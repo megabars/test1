499219d2b71a2e6111b5827e2800e5e0ba00293d
imezencev <>
2024-01-10 22:12:02 +0700

    111

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,6 +17,9 @@ public class Main {
     public static int getMinimizeHeights(int arr[], int n, int k)
     {
         if (n == 1)
+
+
+
             return 0;
         Arrays.sort(arr);
         int diff = arr[n-1] - arr[0];
